5c2593640a5a217b65ad541a7ee07b809ac7439b
prasad.srg <prasad.srg@gmail.com>
2018-01-11 16:35:59 +0530

    commit for swagger

--- src/app.py
+++ src/app.py
@@ -1,54 +1,80 @@
 from flask import Flask, jsonify
 from flask_restful import Api, Resource
-from flask.views import MethodView
 from flask_jwt import JWT
-from flask_swagger import swagger
-#from flasgger import Swagger, swag_from
+from flasgger import Swagger
+from flasgger.utils import swag_from
+
 app = Flask(__name__)
 app.secret_key = 'abcd'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 api = Api(app)
 
+
 #jwt = JWT(app, authenticate, identity)
 
-#from resources.user_resource import UserResource
-@app.route("/user")
-class UserResource():
-    """
-    swagger_from_file: spec/user/save.yml
-    """
-    def get(self):
-        # return { 'status': 1, data : list(map( lambda x: x.json(), ItemModel.query.all() ) ) }
-        # return { 'status': 1, data : [x.json for x in ItemModel.query.all() ] }
-        return {'status': 1, 'data': 'HelloWorld'}
+from resources.user_resource import UserResource
+#
+#
+# class UserResource(Resource):
+#
+#
+#     def get(self):
+#         # return { 'status': 1, data : list(map( lambda x: x.json(), ItemModel.query.all() ) ) }
+#         # return { 'status': 1, data : [x.json for x in ItemModel.query.all() ] }
+#         return {'status': 1, 'data': 'HelloWorld'}
+#
+#
+#     def post(self):
+#         # return { 'status': 1, data : list(map( lambda x: x.json(), ItemModel.query.all() ) ) }
+#         # return { 'status': 1, data : [x.json for x in ItemModel.query.all() ] }
+#         return {'status': 1, 'data': 'HelloWorld'}
 
+api.add_resource(UserResource, '/user')
 
-    def post(self):
-        # return { 'status': 1, data : list(map( lambda x: x.json(), ItemModel.query.all() ) ) }
-        # return { 'status': 1, data : [x.json for x in ItemModel.query.all() ] }
-        return {'status': 1, 'data': 'HelloWorld'}
-
-#api.add_resource(UserResource, '/user')
-
-@app.route("/spec")
-def spec():
-    swag = swagger(app, from_file_keyword='swagger_from_file')
-    swag['info']['version'] = "1.0"
-    swag['info']['title'] = "My API"
-    return jsonify(swag)
-
+@app.after_request
+def after_request(response):
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Headers', "Authorization, Content-Type")
+    response.headers.add('Access-Control-Expose-Headers', "Authorization")
+    response.headers.add('Access-Control-Allow-Methods', "GET, POST, PUT, DELETE, OPTIONS")
+    response.headers.add('Access-Control-Allow-Credentials', "true")
+    response.headers.add('Access-Control-Max-Age', 60 * 60 * 24 * 20)
+    return response
 
 if __name__ == '__main__':
-    @app.after_request
-    def after_request(response):
-        response.headers.add('Access-Control-Allow-Origin', '*')
-        response.headers.add('Access-Control-Allow-Headers', "Authorization, Content-Type")
-        response.headers.add('Access-Control-Expose-Headers', "Authorization")
-        response.headers.add('Access-Control-Allow-Methods', "GET, POST, PUT, DELETE, OPTIONS")
-        response.headers.add('Access-Control-Allow-Credentials', "true")
-        response.headers.add('Access-Control-Max-Age', 60 * 60 * 24 * 20)
-        return response
+    app.config['SWAGGER'] = {
+        "swagger_version": "2.0",
+        "title": "ABCD",
+        "headers": [
+            ('Access-Control-Allow-Origin', '*'),
+            ('Access-Control-Allow-Methods', "GET, POST, PUT, DELETE, OPTIONS"),
+            ('Access-Control-Allow-Credentials', "true"),
+        ],
+    }
+    Swagger(app, template={
+        "swagger": "2.0",
+        "info": {
+            "version": "1.0",
+        },
+        "consumes": [
+            "application/json",
+            "application/x-www-form-urlencoded",
+        ],
+        "produces": [
+            "application/json",
+        ],
+        "securityDefinitions": {
+            "jwt": {
+                "type": 'apiKey',
+                "name": 'Authorization',
+                "in": 'header'
+            }
+        },
+        "security": [
+            {"jwt": []}
+        ]
+    },)
     from db import db
     db.init_app(app)
     app.run(port=5001, debug=True)
--- src/resources/user_resource.py
+++ src/resources/user_resource.py
@@ -1,19 +1,39 @@
-from flask_restful import  Resource, MethodView
+from flask import request, jsonify, json
+from flask_restful import  Resource
 from flask_jwt import jwt_required
-from flask_swagger import swagger
+from flasgger import swag_from
 
-class UserResource(Resource):
 
-    """
-        swagger_from_file: spec/user/save.yml
-    """
-    def get(self):
+class UserResource (Resource):
+
+    @swag_from('../../spec/user/save.yml')
+    def put(self):
+        req_json = json.loads(request.data)
+        req_data = None if 'data' not in req_json else req_json['data']
+
+        res_json = { }
+        res_json['status']=1
+        res_json['data'] = req_data
+        return jsonify(res_json)
+
+    @swag_from('../../spec/user/search.yml')
+    def post(self):
         # return { 'status': 1, data : list(map( lambda x: x.json(), ItemModel.query.all() ) ) }
         # return { 'status': 1, data : [x.json for x in ItemModel.query.all() ] }
         return {'status': 1, 'data': 'HelloWorld'}
 
+    @swag_from('../../spec/user/entity.yml')
+    def get(self):
+        id = request.args['id']
+        res_json = { }
+        res_json['status']=1
+        res_json['data'] = {}
+        res_data =  res_json['data']
+        res_data['id']=id
+        return jsonify(res_json)
 
-    def post(self):
+    @swag_from('../../spec/user/delete.yml')
+    def delete(self):
         # return { 'status': 1, data : list(map( lambda x: x.json(), ItemModel.query.all() ) ) }
         # return { 'status': 1, data : [x.json for x in ItemModel.query.all() ] }
         return {'status': 1, 'data': 'HelloWorld'}
