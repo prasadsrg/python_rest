19f82457a7822752513ffb484ca50d72d2e3e32b
naveenreddysg <naveenreddy.sg@gmail.com>
2018-02-05 13:29:39 +0530

    commit

--- src/app.py
+++ src/app.py
@@ -35,6 +35,7 @@ api.add_resource(ConsumerResource, '/consumer')
 
 from resources.apex_report_data_resource import ApexReportDataResource
 api.add_resource(ApexReportDataResource, '/apex_report_data')
+
 # @app.after_request
 # def after_request(response):
 #     response.headers.add('Access-Control-Allow-Origin', '*')
