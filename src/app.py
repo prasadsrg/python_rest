016cec971a0c13cb64e1346054b20bd72334e488
naveenreddysg <naveenreddy.sg@gmail.com>
2018-02-05 13:24:00 +0530

    apex report data commited

--- src/app.py
+++ src/app.py
@@ -32,6 +32,9 @@ api.add_resource(BranchResource, '/branch')
 
 from resources.consumer_resource import ConsumerResource
 api.add_resource(ConsumerResource, '/consumer')
+
+from resources.apex_report_data_resource import ApexReportDataResource
+api.add_resource(ApexReportDataResource, '/apex_report_data')
 # @app.after_request
 # def after_request(response):
 #     response.headers.add('Access-Control-Allow-Origin', '*')
