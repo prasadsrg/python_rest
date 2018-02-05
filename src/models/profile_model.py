fad2225776b7c7e51cf05e45387a29783dcd7c35
naveenreddysg <naveenreddy.sg@gmail.com>
2018-02-05 16:04:51 +0530

    profile is done

---
+++ src/models/profile_model.py
@@ -0,0 +1,30 @@
+from db import db
+from models.address_model import AddressModel
+from models.img_model import ImgModel
+from models.branch_model import BranchModel
+
+class ProfileModel(db.Model):
+
+    __tablename__ = 'branch'
+
+    id = db.Column('id', db.String, primary_key=True)
+    name = db.Column('name', db.String)
+    phone = db.Column('phone', db.String)
+    mobile = db.Column('mobile', db.String)
+    email = db.Column('email', db.String)
+    aadhar = db.Column('aadhar', db.String)
+    password = db.Column('password', db.String)
+    token = db.Column('token', db.String)
+    role = db.Column('role', db.String)
+    addressId = db.Column('address_id', db.String, db.ForeignKey('address.id'))
+    address = db.relationship(AddressModel)
+    branchId = db.Column('branch_id', db.String, db.ForeignKey('branch.id'))
+    branch = db.relationship(BranchModel)
+    imgId = db.Column('img_id', db.String, db.ForeignKey('img.id'))
+    img = db.relationship(ImgModel)
+    active = db.Column('active', db.Boolean)
+    vid = db.Column('vid', db.String)
+    created_by = db.Column('created_by', db.String)
+    createdOn = db.Column('created_on', db.DateTime)
+    updatedBy= db.Column('updated_by', db.String)
+    updatedOn = db.Column('updated_on', db.DateTime)
