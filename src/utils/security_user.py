15f38564053c94a11e5f3b7cd3429848f5368ab9
prasad.srg <prasad.srg@gmail.com>
2018-02-07 12:47:20 +0530

    commit for auth

--- src/utils/security_user.py
+++ src/utils/security_user.py
@@ -3,6 +3,8 @@ from flask_restful import  Resource
 from flask import request, jsonify, json
 from itsdangerous import (TimedJSONWebSignatureSerializer
                           as Serializer, BadSignature, SignatureExpired)
+from db import db
+from sqlalchemy import text
 
 class JwtIdentify(object):
     def __init__(self, id):
@@ -13,15 +15,49 @@ class SecurityUser(Resource):
 
     @swag_from('../../spec/app/auth.yml', methods=['POST'])
     def post(self):
+        print("inside post")
         pass
 
     @staticmethod
     def authenticate(username, password):
+        param = {}
+        param["vid"] = username.split('-')[0]
+        param["userid"] = username.split('-')[1]
+        param["password"] = password
+        print(param)
+        sql = """
+            select
+                id,
+                name,
+                email,
+                mobile,
+                role,
+                branch_id,
+                vid
+            from profile 
+            where vid = '{vid}'
+            and (email = '{userid}' or mobile = '{userid}')
+            and password = '{password}'
+        """.format(**param)
+        print(sql)
+        print("inside authenticate")
+        result = db.engine.execute(text(sql)).first();
+        print(result)
         user = {}
-        user['id'] = 'SUPPORT_DFF_USER'
-        user['name'] = 'Support User'
-        user['role'] = 'SuperAdmin'
-        user['vid'] = 'DFF'
+        user['id'] = result[0]
+        user['name'] = result[1]
+        user['email'] = result[2]
+        user['mobile'] = result[3]
+        user['role'] = result[4]
+        user['branchId'] = result[5]
+        user['vid'] = result[6]
+        # user['id'] = 'SUPPORT_DFF_USER'
+        # user['name'] = 'Support User'
+        # user['email'] = 'support@dfftech.com'
+        # user['mobile'] = '123456789'
+        # user['role'] = 'SuperAdmin'
+        # user['vid'] = 'DFF'
+        print(user)
         return JwtIdentify(user)
 
     @staticmethod
