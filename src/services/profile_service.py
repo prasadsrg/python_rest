f0601748e071bc2b1faad676cf4a7b4aab303f1c
prasad.srg <prasad.srg@gmail.com>
2018-02-16 12:44:11 +0530

    commit fo session

--- src/services/profile_service.py
+++ src/services/profile_service.py
@@ -1,4 +1,4 @@
-from db import db
+from db import session
 from utils.util import uid
 from models.profile_model import ProfileModel
 from models.branch_model import BranchModel
@@ -17,7 +17,6 @@ class ProfileService:
     def mapping(self, model, view):
         print(self.session_info)
         if model.id is None:
-            model = ProfileModel()
             model.id = uid()
             model.branchId = view['branch']['id']
             model.address = AddressModel()
@@ -36,11 +35,14 @@ class ProfileService:
         AddressMapper(model.address, view.get('address', None)).model_mapping()
         ImgMapper(model.img, view.get('img', None)).model_mapping()
 
-    def isValidate(self, model):
+    def isValidate(self, model, is_new):
         model.vid = self.session_info['vid']
-        list = ProfileModel.query.filter(vid==model.vid, (mobile==model.mobile | email == model.email))
-        if list:
-            if model.id is None:
+        query = session.query(ProfileModel).filter(ProfileModel.vid==model.vid)\
+            .filter((ProfileModel.mobile==model.mobile) | (ProfileModel.email == model.email))
+        data_list = query.all()
+        print(data_list)
+        if data_list:
+            if is_new:
                 return False;
             else:
                 for item in list:
@@ -50,15 +52,18 @@ class ProfileService:
 
     def save(self, req_data):
         profile = None
-        _id = req_data('id', None);
+        _id = req_data.get('id', None);
+        is_new = True
         if _id is not None:
             profile = ProfileModel.query.filter_by(id=_id).first()
+            is_new = False
         if profile is None:
             profile = ProfileModel()
-        if self.isValidate(profile):
-            self.mapping(profile, req_data)
-            db.session.add(profile)
-            db.session.commit()
+            is_new = True
+        self.mapping(profile, req_data)
+        if self.isValidate(profile, is_new):
+            session.add(profile)
+            session.commit()
             return {'message': 'Saved Successfully', 'id': profile.id}
         else:
             raise Exception('Record already exists')
